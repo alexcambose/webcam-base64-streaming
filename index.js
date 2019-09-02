99037dda86d0224a5ad338159ed783f99ef61bde
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2019-09-02 04:02:37 -0400

    Get back to routing

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const path = require('path')
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
 
-const httpServer = http.createServer(express())
+const app = express()
+const httpServer = http.createServer(app)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const wsServer = new WebSocket.Server({ server: httpServer }, () => console.log(`WS server is listening at ws://localhost:${PORT}`));
+const wsServer = new WebSocket.Server({ server: httpServer })
 
 // array of connected websocket clients
-let connectedClients = [];
+let connectedClients = []
 
-wsServer.on('connection', (ws, req) => {
-    console.log('Connected');
-    // add new connected client
-    connectedClients.push(ws);
-    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+wsServer.on('connection', ws => {
+    console.log('Connected') // add new connected client
+
+    connectedClients.push(ws) // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+
     ws.on('message', data => {
-        console.log(data)
-        // send the base64 encoded frame to each connected ws
         connectedClients.forEach((ws, i) => {
             if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
+                ws.send(data) // send
             } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
+                connectedClients.splice(i, 1)
             }
-        });
-    });
-});
+        })
+    })
+})
 
-httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
+// HTTP stuff
+app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')))
+app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './streamer.html')))
+
+httpServer.listen(PORT, () => console.log(`
+    hi,
+    1. Open streamer http://localhost:${PORT}/streamer
+    2. Open client http://localhost:${PORT}/client
+`))
